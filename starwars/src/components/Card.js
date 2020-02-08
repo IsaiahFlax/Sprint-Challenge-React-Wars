83cb1bb389e92dedb4eb5976d985bd008ec34d19
IsaiahFlax <flax.isaiah@gmail.com>
2020-02-07 21:28:40 -0700

    i think i have completed this

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,10 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
+const CardDiv = styled.div`
+        margin: 100px;
+        border: 1px solid black;
+    `;
+const Name = styled.span`
+        text-decoration: underline;
+    `;
+
 
 const Card = props => {
     console.log("card props", props)
     return (
-       `${(props.props.name)}`
-    )
+        <CardDiv>
+            <Name>Name: {props.props.name}</Name>
+            <p>Birth Year: {props.props.birth_year}</p>
+            <p>Height: {props.props.height}</p>
+        </CardDiv>
+        );
 }
 
 export default Card;
